ee89bd3c7178ff58c1d215e7cd6f8b94a473e27d
Yevhen Turovets <yevhen.turovets@gmail.com>
2026-02-16 11:02:36 +0200

    feat(request): create StoreTicketRequest

---
+++ app/Http/Requests/StoreTicketRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTicketRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, array<int, string>>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+        ];
+    }
+}
